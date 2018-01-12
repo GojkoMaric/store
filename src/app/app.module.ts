0bb7f07b5c9c6f48477199650766288b420c7792
GojkoMaric <gojko.maric@yahoo.com>
2018-01-12 19:43:20 +0100

    1 exam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NabBarComponent } from './components/nab-bar/nab-bar.component';
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { ProductsComponent } from './components/products/products.component';
+import { LayoutComponent } from './components/layout/layout.component';
+
+const appRoutes: Routes = [
+  {path: '', component: ProductsComponent},
+  {path: 'buyers', component: BuyersComponent},
+  {path: 'products', component: ProductsComponent}
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabBarComponent,
+    BuyersComponent,
+    ProductsComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
